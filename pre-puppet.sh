223960da3daa1a204f6430bacc32075643a75b7a
Tarnatos <tarnatos@posteo.de>
2017-07-04 13:29:39 +0200

    Aktualisierung auf letzten FFNord Stand

--- pre-puppet.sh
+++ pre-puppet.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 #https://github.com/ffnord/ffnord-puppet-gateway
+# adapt IPV6 first!
+# do dpkg-reconfigure tzdata
 
 NAME="Freifunk Nordheide"
 OPERATOR="heini66"
-CHANGELOG=""
-HOST_PREFIX="gw"
-SUBDOMAIN_PREFIX="gw"
-VPN_NUMBER="00"
+HOST_PREFIX="gw-"
+SUBDOMAIN_PREFIX=vpn
+VPN_NUMBER=11
 DOMAIN="nordheide.freifunk.net"
-SUDOUSERNAME="heini66"
+SUDOUSERNAME="debian"
 TLD=ffnh
 
 #backborts einbauen
 echo "deb http://http.debian.net/debian jessie-backports main" >>/etc/apt/sources.list
 
 #sysupgrade
-apt-get update && apt-get upgrade && apt-get dist-upgrade
-
-#add users:
-useradd -U -G sudo -m $SUDOUSERNAME
+apt-get update && apt-get dist-upgrade && apt-get upgrade
 
 #MOTD setzen
 rm /etc/motd
@@ -27,24 +25,25 @@ echo " $NAME - Gateway $SUBDOMAIN_PREFIX$VPN_NUMBER $NAME " >>/etc/motd
 echo " Hoster: $OPERATOR *" >>/etc/motd
 echo "*******************************************************" >>/etc/motd
 echo " " >>/etc/motd
-echo " Changelog: " >>/etc/motd
-echo " $CHANGELOG " >>/etc/motd
 echo " *" >>/etc/motd
 echo " Happy Hacking! *" >>/etc/motd
 echo "**********************************************************" >>/etc/motd
 
-#OVH
 #Hostname setzen
-#hostname $HOST_PREFIX$VPN_NUMBER
+hostname $HOST_PREFIX$VPN_NUMBER
 #echo "127.0.1.1 $SUBDOMAIN_PREFIX$VPN_NUMBER.$DOMAIN $HOST_PREFIX$VPN_NUMBER" >>/etc/hosts
-#rm /etc/hostname
-#echo "$HOST_PREFIX$VPN_NUMBER" >>/etc/hostname
+rm /etc/hostname
+touch /etc/hostname
+echo "$HOST_PREFIX$VPN_NUMBER" >>/etc/hostname
 
-#benoetigte Pakete installieren
-apt-get -y install sudo apt-transport-https bash-completion haveged git tcpdump mtr-tiny vim nano unp mlocate screen tmux cmake build-essential libcap-dev pkg-config libgps-dev python3 ethtool lsb-release zip locales-all
+# install needed packages
+apt-get -y install sudo apt-transport-https git nload
 
-#REBOOT on Kernel Panic
-echo "kernel.panic = 10" >>/etc/sysctl.conf
+# optional pre installed to speed up the setup:
+apt-get -y install bash-completion haveged sshguard tcpdump mtr-tiny vim nano unp mlocate screen tmux cmake build-essential libcap-dev pkg-config libgps-dev python3 ethtool lsb-release zip locales-all ccze ncdu
+
+#not needed packages from standard OVH template
+apt-get -y remove nginx nginx-full exim mutt
 
 #puppet modules install
 apt-get -y install --no-install-recommends puppet
@@ -56,6 +55,28 @@ puppet module install torrancew-account --version 0.1.0
 cd /etc/puppet/modules
 git clone https://github.com/ffnord/ffnord-puppet-gateway ffnord
 
+# symlink check-install script
+ln -s /etc/puppet/modules/ffnord/files/usr/local/bin/check-services /usr/local/bin/check-services
+
+# add aliases
+cat <<-EOF>> /root/.bashrc
+  export LS_OPTIONS='--color=auto'
+  eval " \`dircolors\`"
+  alias ls='ls \$LS_OPTIONS'
+  alias ll='ls \$LS_OPTIONS -lah'
+  alias l='ls \$LS_OPTIONS -lA'
+  alias grep="grep --color=auto"
+  # let us only use aptitude on gateways
+  alias apt-get='sudo aptitude'
+  alias ..="cd .."
+  # set nano to (S=)smooth scrolling and (i=)autoindent (T=)2 tabs (E=)as spaces
+  alias nano='nano -S -i -T2 -E'
+  # history settings
+  HISTCONTROL=ignoreboth
+  HISTSIZE=10000
+  HISTFILESIZE=200000
+EOF
+
 # back in /root
 cd /root
 
@@ -63,8 +84,37 @@ echo load the ip_tables and ip_conntrack module
 modprobe ip_conntrack
 echo ip_conntrack >> /etc/modules
 
+#SSH config
+rm /etc/ssh/sshd_config
+cp /opt/nordheide-puppet-config/sshd_config /etc/ssh/sshd_config
+service sshd restart
+
+#online script
+touch /usr/local/bin/online
+cat <<-EOF>> /usr/local/bin/online
+#!/bin/bash
+maintenance off && service ntp start && batctl -m bat-ffnord gw server 100000/100000 && check-services
+EOF
+chmod +x /usr/local/bin/online
+
+#OVH network config
+cat <<-EOF>> /etc/network/interfaces
+iface eth0 inet6 static
+       address 2001:41d0:401:2000::2:f0bd
+       netmask 128
+       post-up /sbin/ip -6 route add 2001:41d0:401:2000::1 dev eth0
+       post-up /sbin/ip -6 route add default via 2001:41d0:401:2000::1 dev eth0
+       pre-down /sbin/ip -6 route del default via 2001:41d0:401:2000::1 dev eth0
+       pre-down /sbin/ip -6 route del 2001:41d0:401:2000::1 dev eth0
+auto eth1
+allow-hotplug eth1
+iface eth1 inet dhcp
+EOF
+
+
 #USER TODO:
 echo 'now copy the files manifest.pp and mesh_peerings.yaml to /root and make sure /root/fastd_secret.key exists'
+echo 'adapt IPV6 adress'
 echo '####################################################################################'
 echo '########### don´t run the following scripts without screen sesssion!!! #############'
 echo '####################################################################################'
